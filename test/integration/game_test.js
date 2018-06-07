0d599bf2681ae36153f8c3488c8db93382b40b7c
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-06-07 17:12:16 +0100

    Add integration tests for app

--- test/integration/game_test.js
+++ test/integration/game_test.js
@@ -1,13 +1,163 @@
 const chai = require('chai');
 const expect = chai.expect;
+const fs = require('fs');
+const path = require('path');
 
 const app = require('../../src/app');
 const gameState = require('../../src/league');
 
 describe('league app', function () {
-  it('prints empty game state', function () {
+  it('gets empty game state', function () {
     const game = app.startGame(gameState.createLeague());
 
     expect(game.sendCommand('print')).to.equal('No players yet');
+  });
+
+  it('adds a player to the game', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Bob');
+
+    expect(game.sendCommand('print')).to.equal('-------------------\n|       Bob       |\n-------------------');
+  });
+
+  it('adds players to rows in a pyramid shape', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Alice');
+    game.sendCommand('add player Bob');
+    game.sendCommand('add player Charlotte');
+    game.sendCommand('add player DAngelo');
+
+    expect(game.sendCommand('print')).to.equal(
+`                    -------------------
+                    |      Alice      |
+                    -------------------
+          ------------------- -------------------
+          |       Bob       | |    Charlotte    |
+          ------------------- -------------------
+------------------- ------------------- -------------------
+|     DAngelo     | |                 | |                 |
+------------------- ------------------- -------------------`
+    );
+  });
+
+  it('shows the current winner', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Alice');
+    game.sendCommand('add player Bob');
+
+    expect(game.sendCommand('winner')).to.equal('Alice');
+  });
+
+  it('swaps players when a lower player wins a game', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Anil');
+    game.sendCommand('add player Brian');
+    game.sendCommand('add player Cara');
+
+    game.sendCommand('record win Cara Anil');
+
+    expect(game.sendCommand('print')).to.equal(
+`          -------------------
+          |      Cara       |
+          -------------------
+------------------- -------------------
+|      Brian      | |      Anil       |
+------------------- -------------------`
+    );
+  });
+
+  it('rejects an invalid win', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Anil');
+    game.sendCommand('add player Brian');
+    game.sendCommand('add player Cara');
+
+    game.sendCommand('record win Anil Cara');
+
+    expect(game.sendCommand('print')).to.equal(
+`          -------------------
+          |      Anil       |
+          -------------------
+------------------- -------------------
+|      Brian      | |      Cara       |
+------------------- -------------------`
+    );
+  });
+
+  describe('save to disk', function () {
+    const tempDir = path.resolve(__dirname, 'tmp')
+
+    before(function () {
+      if (!fs.existsSync(tempDir)) {
+        fs.mkdirSync(tempDir);
+      }
+    });
+
+    afterEach(function () {
+      fs.readdir(tempDir, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+          fs.unlink(path.join(tempDir, file), err => {
+            if (err) throw err;
+          });
+        }
+      })
+    });
+
+    it('stores an empty game state to disk', function () {
+      const game = app.startGame(gameState.createLeague());
+      const absolutePath = getTemporaryFilePath('empty_game.json');
+
+      game.sendCommand(`save "${absolutePath}"`);
+
+      const fileContents = JSON.parse(fs.readFileSync(absolutePath, 'utf8'));
+      expect(fileContents).to.eql([]);
+    });
+
+    it('stores an in-progress game state to disk', function () {
+      const game = app.startGame(gameState.createLeague());
+      game.sendCommand('add player Alec');
+      game.sendCommand('add player Barry');
+      game.sendCommand('add player Cip');
+      game.sendCommand('add player Deniz');
+
+      const absolutePath = getTemporaryFilePath('in_progress_game.json');
+
+      game.sendCommand(`save "${absolutePath}"`);
+
+        const fileContents = JSON.parse(fs.readFileSync(absolutePath, 'utf8'));
+      expect(fileContents).to.eql([
+        ['Alec'],
+        ['Barry', 'Cip'],
+        ['Deniz']
+      ]);
+    });
+
+    function getTemporaryFilePath (name) {
+      return path.resolve(__dirname, `tmp/${name}`);
+    }
+  });
+
+  describe('load from disk', function () {
+    it('loads an in-progress game', function () {
+      const gameFile = path.resolve(__dirname, '../fixtures/in_progress_game.json');
+
+      const game = app.startGame(gameState.createLeague());
+
+      game.sendCommand(`load "${gameFile}"`);
+
+      expect(game.sendCommand('print')).to.equal(
+`                    -------------------
+                    |       Amy       |
+                    -------------------
+          ------------------- -------------------
+          |    Berenice     | |     Carole      |
+          ------------------- -------------------
+------------------- ------------------- -------------------
+|      Darla      | |                 | |                 |
+------------------- ------------------- -------------------`
+      );
+    });
   });
 });
