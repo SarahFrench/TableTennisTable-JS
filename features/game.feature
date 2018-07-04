9e67d51e69052b49078c800308a75fc018a299d1
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-07-04 15:12:14 +0100

    Add feature test for adding a player

--- features/game.feature
+++ features/game.feature
@@ -4,3 +4,15 @@ Feature: Run a table tennis league
     Given the league has no players
     When I print the league
     Then I should see that there are no players
+
+  Scenario: adding players
+    Given the league has players:
+      | Alice   |
+      | Bob     |
+      | Charles |
+      | Dana    |
+    When I print the league
+    Then I should see "Alice" in row 1
+    And I should see "Bob" in row 2
+    And I should see "Charles" in row 2
+    And I should see "Dana" in row 3
--- features/support/steps.js
+++ features/support/steps.js
@@ -5,10 +5,28 @@ Given('the league has no players', function () {
   // Nothing to do because a new league always has no players
 });
 
+Given('the league has players:', function (data) {
+  const game = this.game;
+  data.raw().forEach(function (tableRow) {
+    const name = tableRow[0];
+    game.sendCommand(`add player ${name}`);
+  });
+});
+
 When('I print the league', function () {
   this.response = this.game.sendCommand('print');
 });
 
 Then('I should see that there are no players', function () {
   expect(this.response).to.equal('No players yet');
+});
+
+Then('I should see {string} in row {int}', function (player, rowNumber) {
+  const outputLines = this.response.split('\n');
+  const playerLineIndex = 3 * rowNumber - 2;
+  const playerLine = outputLines[playerLineIndex];
+
+  const actualNames = playerLine.match(/\w+/g);
+
+  expect(actualNames).to.contain(player);
 });
